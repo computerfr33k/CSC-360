286b1f043b0f2347c57ff568e372afb0b35dd24c
Computerfr33k <computerfreak@computerfr33k.com>
2015-04-07 14:41:36 -0500

    Added CSS to Tip Calc

--- In Class/Tip Calculator/tip-calculator.js
+++ In Class/Tip Calculator/tip-calculator.js
@@ -1,6 +1,6 @@
 function updateTotal() {
-	var tip = Number(document.getElementById("service-rating").value.trim());
-	var check = Number(document.getElementById("check-amount").value.trim());
+	var tip = Number($("#service-rating").val());
+	var check = Number($("#check-amount").val());
 	
 	var tipTotal = (check * (tip/100));
 	document.getElementById("tip-value").innerText = tipTotal.toFixed(2);
@@ -8,3 +8,7 @@ function updateTotal() {
 	var totalAmount = check + tipTotal;
 	document.getElementById("total-value").innerText = totalAmount.toFixed(2);
 }
+
+$(document).ready(function() {
+	$("#calc").attr("onClick", "updateTotal();");
+});
